e4375b29c558cc907ec2368cb32c09a184f1eea3
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-17 21:48:54 -0400

    Open linkedin OAuth2 screen.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+import open from "open";
 
-console.log("Hello world from my first NPM package! Test 2");
+(async () => {
+  const clientId = "86x9b3v7je99at";
+  const redirectURI = "https%3A%2F%2Fwww.google.com%2F";
+
+  await open(
+    `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectURI}&scope=w_member_social`
+  );
+
+  console.log("done");
+})();
